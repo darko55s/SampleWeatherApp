440d923eb017f8ee6fa7a7a4dd04269bbb011123
Darko Spasovski <spasovski.darko@gmail.com>
2019-04-18 23:41:57 +0200

    Inicial Commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SampleWeatherApp' do
+  use_frameworks!
+
+  pod 'IGListKit', '~> 3.0'
+  pod 'Alamofire'
+end
---
+++ SampleWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleWeatherApp
+//
+//  Created by Darko Spasovski on 4/18/19.
+//  Copyright © 2019 Darko Spasovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
